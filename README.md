d0907505e27aceac9c36c6ef1bb1e33265b1ddc7
wjhwang <hwangwonjun@gmail.com>
2018-07-04 17:00:28 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+
+## How to run
+
+After having cloned this repository, run the following commands at the this root:
+
+```js
+yarn install // or npm install
+
+yarn start // or npm start
+```
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm start`
+
+Runs the app in the development mode.<br>
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+## Reference Link
+
+- [React-Bootstrap](https://react-bootstrap.github.io/)
+
+- [React Doc](https://reactjs.org/docs/getting-started.html);
---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import { Nav, Navbar, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <Navbar>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <a href="#home">React-Bootstrap</a>
+          </Navbar.Brand>
+        </Navbar.Header>
+        <Nav>
+          <NavItem eventKey={1} href="#">
+            Link
+          </NavItem>
+          <NavItem eventKey={2} href="#">
+            Link
+          </NavItem>
+          <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+            <MenuItem eventKey={3.1}>Action</MenuItem>
+            <MenuItem eventKey={3.2}>Another action</MenuItem>
+            <MenuItem eventKey={3.3}>Something else here</MenuItem>
+            <MenuItem divider />
+            <MenuItem eventKey={3.4}>Separated link</MenuItem>
+          </NavDropdown>
+        </Nav>
+      </Navbar>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
